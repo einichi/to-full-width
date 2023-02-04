fb24f7200fcc261fda93b55fa783e0d0908ba8d2
einichi <ricky@burg.in>
2023-02-04 19:14:01 +0900

    update coverage test

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,11 @@
 export default {
   collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
   coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 };
